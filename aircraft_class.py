c9314bd18ec7da3dabd9c543a3ce510967d7d9b9
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2020-03-11 08:22:53 +0000

    class completed

--- aircraft_class.py
+++ aircraft_class.py
@@ -2,3 +2,16 @@
 # attributes: cargo
 
 # Define a couple methods; accelerate and brake
+
+
+class Aircraft:
+    def __init__(self, cargo):
+        self.cargo = cargo
+
+
+    def accelerate(self):
+        return "Go Forward"
+
+    def brake(self):
+        return "Stop moving"
+
