d1074e71350960f4229f368ce11077ab9591bfdb
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2020-03-11 08:28:00 +0000

    class and function

---
+++ helicopter_class.py
@@ -0,0 +1,22 @@
+from aircraft_class import *
+
+class Helicopter(Aircraft):
+    def __init__(self, cargo, number_of_blades):
+        super().__init__(cargo)
+        self.number_of_blades = number_of_blades
+
+
+def helicopter():
+    while True:
+        print("Creating helicopter")
+        ask1 = input("How many blades does the helicopter have? \n"
+                     "> "
+                     )
+        ask2 = input("What is the helicopter carrying? \n"
+                     "> ")
+        helicopter = Helicopter(ask2, ask1)
+        print(f"Helicopter with {helicopter.number_of_blades} blades carrying {helicopter.cargo} has been created.")
+        ask3 = input("Make another helicopter? Y/N \n"
+                     "> ")
+        if ask3.upper() == "N":
+            break
