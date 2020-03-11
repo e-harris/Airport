9e4e1b6f373310224194d42f8c1376dc9ac1100e
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2020-03-11 08:22:15 +0000

    class and user stories completed

--- plane_class.py
+++ plane_class.py
@@ -1,3 +1,4 @@
+from aircraft_class import *
 # define plane class
 # it inherits from aircraft
 
@@ -7,3 +8,26 @@
 
 # Plane
 # As a User I can create a Plane with a plane number
+
+
+class Plane(Aircraft):
+    def __init__(self, cargo, plane_number):
+        super().__init__(cargo)
+        self.plane_number = plane_number
+
+
+
+def create_plane():
+    while True:
+        print("Creating plane")
+        ask1 = input("What is the code of the plane? \n"
+                     "> "
+                     )
+        ask2 = input("What is the plane carrying? \n"
+                     "> ")
+        plane = Plane(ask1, ask2)
+        print(f"Plane {plane.plane_number} carrying {plane.cargo} has been created.")
+        ask3 = input("Make another plane? Y/N \n"
+                     "> ")
+        if ask3.upper() == "N":
+            break
